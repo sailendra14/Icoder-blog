7dacfc92fbdea3dbf42068ca71cf90ce9c191fbc
sailendra14 <sailendraindia14@gmail.com>
2020-07-15 11:16:47 +0530

    vid 83 done

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from home.models import Contact
 from django.contrib import messages
+from blog.models import Post
 
 # Create your views here.
 def home(request):
@@ -24,3 +25,9 @@ def contact(request):
             contact.save()
             messages.success(request, 'Your message has been successfully sent!')
     return render(request, 'home/contact.html')
+
+def search(request):
+    query = request.GET['query']
+    allPosts = Post.objects.filter(title__icontains=query)
+    params = {'allPosts': allPosts}
+    return render(request, 'home/search.html', params)
