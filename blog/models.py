7e3d445d56c4d5b9b4459fc581079f41847112c4
sailendra14 <sailendraindia14@gmail.com>
2020-07-15 08:27:18 +0530

    vid 81 completed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    sno = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=80)
+    author = models.CharField(max_length=20)
+    slug = models.CharField(max_length=120)
+    content = models.TextField()
+    timeStamp = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title + ' - ' + self.author
