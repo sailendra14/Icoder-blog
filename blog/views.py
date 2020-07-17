13a492ce7a29bd3c6de9d1aebff107f034ac2201
sailendra14 <sailendraindia14@gmail.com>
2020-07-17 10:36:33 +0530

    vid 95 completed

--- blog/views.py
+++ blog/views.py
@@ -4,27 +4,37 @@ from blog.models import Post, BlogComment
 from django.contrib import messages
 
 # Create your views here.
+
+
 def blogHome(request):
     allPosts = Post.objects.all()
     context = {'allPosts': allPosts}
     return render(request, 'blog/blogHome.html', context)
 
+
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
     comments = BlogComment.objects.filter(post=post)
-    context = {'post': post, 'comments':comments, 'user': request.user}
+    context = {'post': post, 'comments': comments, 'user': request.user}
     return render(request, 'blog/blogPost.html', context)
+
 
 def postComment(request):
     if request.method == 'POST':
         comment = request.POST.get('comment')
         user = request.user
-        postSno = request.POST.get('postSno') 
+        postSno = request.POST.get('postSno')
         post = Post.objects.get(sno=postSno)
+        parentSno = request.POST.get('parentSno')
 
-        comment = BlogComment(comment=comment, user=user, post=post)
-        comment.save()
-        messages.success(request, 'Your comment has been posted successfully')
-        
+        if parentSno == "":
+            comment = BlogComment(comment=comment, user=user, post=post)
+            comment.save()
+            messages.success(request, 'Your comment has been posted successfully')
+        else:
+            parent = BlogComment.objects.get(sno=parentSno)
+            comment = BlogComment(comment=comment, user=user, post=post, parent=parent)
+            comment.save()
+            messages.success(request, 'Your reply has been posted successfully')
 
     return redirect(f'/blog/{post.slug}')
